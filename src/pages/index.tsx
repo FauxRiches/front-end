17c5fc041f6593edd7e30b2b78fc15eb7f4ca573
oltean-petru <poltean33@gmail.com>
2024-05-30 15:00:36 +0200

    feat: login page ui
    Co-authored-by: Kévin <Oui-Dev@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,47 +10,31 @@ export default function Home() {
           aria-label="Global"
         >
           {/* <a href="#" className="block -m-1.5 p-1.5"> */}
-            <Image
-              src="/images/main_logo.png"
-              alt="FauxRiches logo"
-              width={32}
-              height={32}
-              className="rounded-full"
-            />
+          <Image
+            src="/images/logo.webp"
+            alt="FauxRiches logo"
+            width={32}
+            height={32}
+            className="rounded-full"
+          />
           {/* </a> */}
-          <a href="#" className="block text-sm font-semibold leading-6 text-white">
+          <Link href="/login" className="block text-sm font-semibold leading-6 text-white">
             Se connecter <span aria-hidden="true">&rarr;</span>
-          </a>
+          </Link>
         </nav>
       </header>
       <main className="relative isolate h-full overflow-hidden bg-gray-900 pb-16 pt-14 sm:pb-20">
         <Image
           src="/images/concert-background.avif"
           alt=""
-          fill={true}
+          fill
           className="absolute inset-0 -z-10 brightness-50 blur-sm object-cover"
         />
-        <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-          />
-        </div>
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-            <div className="sm:mb-8 sm:flex sm:justify-center">
+            <div className="mb-8 flex justify-center">
               <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
-                Retrouvez nous le vendredi 21 juin 2024 à Lyon.{" "}
-                <a href="#" className="font-semibold text-white">
-                  <span className="absolute inset-0" aria-hidden="true" />
-                  En savoir plus <span aria-hidden="true">&rarr;</span>
-                </a>
+                Retrouvez nous le vendredi 21 juin 2024 à Lyon.
               </div>
             </div>
             <div className="text-center">
---
+++ src/pages/login.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <main className="inline-flex w-full min-h-[100svh]">
+      <div className="mx-auto w-full max-w-xl flex flex-col justify-center px-4 py-12 sm:px-6 lg:px-20 xl:px-24">
+        <Link href="/" className="block w-fit">
+          <Image
+            src="/images/logo.webp"
+            alt="FauxRiches logo"
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
+        </Link>
+        <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Se connecter à votre compte
+        </h2>
+
+        <form action="#" method="POST" className="space-y-6 mt-10">
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+              Adresse email
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+              Mot de passe
+            </label>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+              />
+              <label htmlFor="remember-me" className="ml-3 block text-sm leading-6 text-gray-700">
+                Se souvenir de moi
+              </label>
+            </div>
+
+            <div className="text-sm leading-6">
+              <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                Mot de passe oublié ?
+              </a>
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Se connecter
+          </button>
+        </form>
+
+        <div className="flex justify-center items-center gap-4 mt-8">
+          <span className="w-full h-px border-t border-gray-200 hidden xs:block" />
+          <Link href="/" className="text-gray-900 whitespace-nowrap text-sm font-medium leading-6">
+            Retour en arrière
+          </Link>
+          <span className="w-full h-px border-t border-gray-200 hidden xs:block" />
+        </div>
+      </div>
+      <div className="relative hidden lg:block w-full max-w-[50%]">
+        <Image
+          src="/images/poster.png"
+          alt=""
+          fill
+          sizes="(min-width: 1024px) 50vw"
+          priority
+          className="object-cover"
+        />
+      </div>
+    </main>
+  )
+}
