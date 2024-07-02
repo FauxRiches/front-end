8a24ae5df78a5ed98d230c765b1fa9d8a9301339
Kévin <kevin.blaise24@gmail.com>
2024-07-02 16:44:41 +0200

    feat: improve roulette initialization

--- src/components/roulette.tsx
+++ src/components/roulette.tsx
@@ -1,67 +1,39 @@
-import React, { useEffect, useRef } from "react";
+import Image from "next/image";
+import React, { useRef } from "react";
 import { Button } from "./ui/button";
 
 const RouletteWheel = ({ state, dispatch }) => {
   const wheelRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    initWheel();
-  }, []);
-
-  const initWheel = () => {
-    const wheel = wheelRef.current;
-    const images = {
-      "0": "/images/pile-of-money.webp",
-      "1": "/images/sport-car.webp",
-      "2": "/images/private-jet.png",
-      "3": "/images/logo.webp",
-      "4": "/images/diamond.png",
-      "5": "/images/ipad.png",
-      "6": "/images/rolex.png",
-      "7": "/images/necless.png",
-      "8": "/images/gold-bar.png",
-      "9": "/images/pile-of-money.webp",
-      "10": "/images/logo.webp",
-      "11": "/images/rolex.png",
-      "12": "/images/gold-bar.png",
-      "13": "/images/diamond.png",
-      "14": "/images/macbook.png",
-    };
-    if (wheel) {
-      let row = `
-      <div class='row flex'>
-        <div class='card'><img class="max-h-[90%]" src='${images["1"]}' alt='1'/></div>
-        <div class='card'><img class="max-h-[90%]" src='${images["14"]}' alt='14'/></div>
-        <div class='card'><img class="max-h-[90%]" src='${images["2"]}' alt='2'/></div>
-        <div class='card'><img class="max-h-[90%]" src='${images["13"]}' alt='13'/></div>
-        <div class='card'><img class="max-h-[90%]" src='${images["3"]}' alt='3'/></div>
-        <div class='card'><img class="max-h-[90%]" src='${images["12"]}' alt='12'/></div>
-        <div class='card'><img class="max-h-[90%]" src='${images["4"]}' alt='4'/></div>
-        <div class='card'><img class="max-h-[90%]" src='${images["0"]}' alt='0'/></div>
-        <div class='card'><img class="max-h-[90%]" src='${images["11"]}' alt='11'/></div>
-        <div class='card'><img class="max-h-[90%]" src='${images["5"]}' alt='5'/></div>
-        <div class='card'><img class="max-h-[90%]" src='${images["10"]}' alt='10'/></div>
-        <div class='card'><img class="max-h-[90%]" src='${images["6"]}' alt='6'/></div>
-        <div class='card'><img class="max-h-[90%]" src='${images["9"]}' alt='9'/></div>
-        <div class='card'><img class="max-h-[90%]" src='${images["7"]}' alt='7'/></div>
-        <div class='card'><img class="max-h-[90%]" src='${images["8"]}' alt='8'/></div>
-      </div>
-      `;
-      for (let x = 0; x < 29; x++) {
-        wheel.innerHTML += row;
-      }
-    }
+  const displayOrder = [0, 11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4];
+  const images = {
+    "0": "/images/roulette/pile-of-money.webp",
+    "1": "/images/roulette/sport-car.webp",
+    "2": "/images/roulette/private-jet.png",
+    "3": "/images/logo.webp",
+    "4": "/images/roulette/diamond.png",
+    "5": "/images/roulette/ipad.png",
+    "6": "/images/roulette/rolex.png",
+    "7": "/images/roulette/necless.png",
+    "8": "/images/roulette/gold-bar.png",
+    "9": "/images/roulette/pile-of-money.webp",
+    "10": "/images/logo.webp",
+    "11": "/images/roulette/rolex.png",
+    "12": "/images/roulette/gold-bar.png",
+    "13": "/images/roulette/diamond.png",
+    "14": "/images/roulette/macbook.png",
   };
 
   const spinWheel = (roll: number = 0) => {
     dispatch({ type: "START_SPIN" });
     const wheel = wheelRef.current;
-    if (wheel) {
-      const order = [0, 11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4];
-      const position = order.indexOf(roll);
 
+    if (wheel) {
       const rows = 12;
       const card = 75 + 3 * 2;
+      const initialCard = 8;
+      const position = (displayOrder.indexOf(roll) + 1 - initialCard);
+
       let landingPosition = rows * 15 * card + position * card;
 
       const randomize = Math.floor(Math.random() * 75) - 75 / 2;
@@ -91,15 +63,34 @@ const RouletteWheel = ({ state, dispatch }) => {
   };
 
   const handleClick = () => {
-    let num = Math.round(Math.random()) ? 3 : 10;
-    spinWheel(num);
+    // Make the wheel result on 3 or 10 randomly
+    spinWheel(Math.random() < 0.5 ? 3 : 10);
   };
 
   return (
     <>
-      <div className="roulette-wrapper relative flex justify-center w-full mx-auto overflow-hidden mb-8">
+      <div className="relative flex justify-center w-full mx-auto overflow-hidden mb-8">
         <div className="w-1 bg-primary absolute left-1/2 h-full -translate-x-1/2 z-10" />
-        <div className="wheel flex" ref={wheelRef}></div>
+        <div className="flex gap-1.5" ref={wheelRef}>
+          {Array.from({ length: 29 }).map((_, i) => (
+            <div key={i} className="flex gap-1.5">
+              {displayOrder.map((item, j) => (
+                <div
+                  key={i + j}
+                  className="relative h-[75px] w-[75px] rounded-lg flex items-center justify-center select-none bg-muted border border-primary shadow-sm shadow-secondary-foreground"
+                >
+                  <Image
+                    src={images[item]}
+                    alt={item.toString()}
+                    fill={true}
+                    sizes="100%"
+                    className="p-[5px] object-contain"
+                  />
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
       <Button onClick={handleClick} disabled={state.isSpinning}>
         Spin
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 // pages/_app.js
 import '../styles/globals.css';
-import '../styles/roulette.css';
 import { ThemeProvider } from 'next-themes';
 
 function App({ Component, pageProps }) {
